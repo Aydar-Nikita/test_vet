7afb2de9da6869f53ed49a43812fd31d22ea3e4e
айдар сайфуллин <sajfullin870@gmail.com>
2022-11-24 10:28:52 +0500

    Добавьте файлы проекта.

---
+++ test_vet/auto.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace test_vet
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string a = log.ToString();
+            string b = pass.ToString();
+            if (a != "" && b != "")
+            {
+                if (a == "admin" && b == "admin")
+                {
+                    MessageBox.Show("Успешно");
+                }
+                else
+                {
+                    MessageBox.Show("Ошибка");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Поля пустые");
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+    }
+}
